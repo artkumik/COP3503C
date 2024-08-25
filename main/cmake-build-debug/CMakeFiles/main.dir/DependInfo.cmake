fe3ddb09c9e1d6dbab20c1e386fc68e96f8d33cf
artkumik <63212694+artkumik@users.noreply.github.com>
2024-08-25 18:33:09 -0400

    4

--- main/cmake-build-debug/CMakeFiles/main.dir/DependInfo.cmake
+++ main/cmake-build-debug/CMakeFiles/main.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/artku/Documents/Github/COP3503C/main/main.cpp" "CMakeFiles/main.dir/main.cpp.obj" "gcc" "CMakeFiles/main.dir/main.cpp.obj.d"
+  "/home/artur-kumik/Documents/GitHub/COP3503C/main/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
